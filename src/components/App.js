9fb89d1a632951ea8a43f864a55be146f0f857a2
liemle <congliemle@gmail.com>
2020-06-14 21:56:08 +0700

    add MovieThumb and Grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Header from "../components/elements/Header";
 import Home from "./Home";
+import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <div>
+    <>
       <Header />
       <Home />
-    </div>
+      <GlobalStyle />
+    </>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import { useHomeFetch } from "./hooks/useHomeFetch";
 import HeroImage from "./elements/HeroImage";
-import { BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
+import Spinner from "./elements/Spinner";
+import Grid from "./elements/Grid";
+import MovieThumb from "./elements/MovieThumb";
+import NoImage from "./images/no_image.jpg";
+
+import { BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 function Home() {
   const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  const [searchTerm, setSearchTerm] = useState("");
   console.log(state);
 
   if (error) return <div>Ooopssss</div>;
-  if (loading || !state.movies[0]) return <div>Loading...</div>;
+  if (!state.movies[0]) return <Spinner />;
 
   return (
     <div>
@@ -17,6 +23,22 @@ function Home() {
         title={state.heroImage.original_title}
         text={state.heroImage.overview}
       />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+        {state.movies.map((movie) => (
+          <MovieThumb
+            key={movie.id}
+            clickable
+            image={
+              movie.poster_path
+                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                : NoImage
+            }
+            movieId={movie.id}
+            movieName={movie.original_title}
+          />
+        ))}
+      </Grid>
+      {loading && <Spinner />}
     </div>
   );
 }
