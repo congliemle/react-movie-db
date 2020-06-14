f104dc39b7ec8c5ec31e1f1dc2a0c468be77e64d
liemle <congliemle@gmail.com>
2020-06-14 20:48:48 +0700

    add usehomeFetch hook and home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
+import { useHomeFetch } from "./hooks/useHomeFetch";
+import HeroImage from "./elements/HeroImage";
+import { BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 
 function Home() {
-  const [state, setState] = useState({ movies: [] });
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+  const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+  console.log(state);
 
-  const fetchMovie = async (endpoint) => {
-    setLoading(true);
-    setError(false);
+  if (error) return <div>Ooopssss</div>;
+  if (loading || !state.movies[0]) return <div>Loading...</div>;
 
-    try {
-      const result = await (await fetch(endpoint)).json();
-    } catch (error) {}
-  };
-
-  return <div></div>;
+  return (
+    <div>
+      <HeroImage
+        image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+        title={state.heroImage.original_title}
+        text={state.heroImage.overview}
+      />
+    </div>
+  );
 }
 
 export default Home;
--- src/config.js
+++ src/config.js
@@ -1,7 +1,15 @@
 const API_URL = "https://api.themoviedb.org/3/";
-const API_KEY = "844dba0bfd8f3a4f3799f6130ef9e335";
+const API_KEY = "3172b42e26017ef7919258cd930f29a5";
 const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 const BACKDROP_SIZE = "w1280";
 const POSTER_SIZE = "w500";
+const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${API_KEY}`;
 
-export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
+export {
+  API_URL,
+  API_KEY,
+  IMAGE_BASE_URL,
+  BACKDROP_SIZE,
+  POSTER_SIZE,
+  POPULAR_BASE_URL,
+};
